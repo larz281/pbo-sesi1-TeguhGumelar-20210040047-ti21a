0db839ce72697b834215f6467c52e45b6d0bc7c1
larz281 <114352578+larz281@users.noreply.github.com>
2022-09-26 00:55:51 +0700

    Add files via upload

---
+++ sesi1no1.java
@@ -0,0 +1,11 @@
+//teguh gumelar
+//20210040047
+//ti21a
+//pbo sesi 1 nomer 1
+public class sesi1no1 {
+    public static void main(String[] args) {
+        System.out.println("Belajar Java");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+}
---
+++ sesi1no2.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JOptionPane;
+
+//teguh gumelar
+//20210040047
+//ti21a
+//pbo sesi 1 nomer 2
+public class sesi1no2 {
+    public static void main(String[] args) {
+   String  belajar;
+   belajar  =   JOptionPane.showInputDialog("Anda sedang belajar apa?");
+   
+        System.out.println("belajar "+belajar+" sangat mudah");
+    }
+}
